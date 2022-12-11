83fa389fe499aaa808ff7448093a9b32162b10b4
Baris2641 <isikbaris188@gmail.com>
2022-12-12 00:53:53 +0300

    asd

---
+++ ecommerce-demo-project-master/src/main/java/com/etiya/ecommercedemo3/business/concretes/CityManager.java
@@ -0,0 +1,41 @@
+package com.etiya.ecommercedemo3.business.concretes;
+
+import com.etiya.ecommercedemo3.business.abstracts.CityService;
+import com.etiya.ecommercedemo3.business.constants.Messages;
+import com.etiya.ecommercedemo3.business.dtos.request.city.AddCityRequest;
+import com.etiya.ecommercedemo3.business.dtos.response.city.AddCityResponse;
+import com.etiya.ecommercedemo3.core.util.mapping.ModelMapperService;
+import com.etiya.ecommercedemo3.core.util.result.DataResult;
+import com.etiya.ecommercedemo3.core.util.result.SuccessDataResult;
+import com.etiya.ecommercedemo3.entities.concretes.City;
+import com.etiya.ecommercedemo3.repository.abstracts.CityRepository;
+import lombok.AllArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+@AllArgsConstructor
+public class CityManager implements CityService {
+    private CityRepository cityRepository;
+
+    private ModelMapperService modelMapperService;
+
+    @Override
+    public DataResult<City> getById(int id) {
+        City response=  cityRepository.findById(id).orElseThrow();
+        return new SuccessDataResult<City>(response, Messages.City.CityGetById) ;
+    }
+
+    @Override
+    public DataResult<AddCityResponse> addCity(AddCityRequest addCityRequest) {
+//        City city = new City();
+//        city.setName(addCityRequest.getName());
+
+        City city= modelMapperService.getMapperRequest().map(addCityRequest,City.class);
+        City savedCity = cityRepository.save(city);
+        AddCityResponse response=modelMapperService.getMapperResponse().map(savedCity,AddCityResponse.class);
+//        AddCityResponse response = new AddCityResponse(savedCity.getId(), savedCity.getName());
+        return new SuccessDataResult<AddCityResponse>(response,Messages.City.CityAddSuccessMessage);
+    }
+}
